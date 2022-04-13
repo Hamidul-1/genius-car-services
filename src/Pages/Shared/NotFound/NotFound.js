5dd0c7bcc9b72c3ac1123898b26ac64926119271
Hamidul-1 <hamidul786cse@gmail.com>
2022-04-14 02:53:22 +0600

    Login, Register from create

---
+++ src/Pages/Shared/NotFound/NotFound.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import sleeping from '../../../images/slipping404.jpg'
+
+const NotFound = () => {
+    return (
+        <div>
+            <h2 className='text-primary text-center'>Mechanic is sleeping</h2>
+            <img className='w-100' src={sleeping} alt="" />
+        </div>
+    );
+};
+
+export default NotFound;
