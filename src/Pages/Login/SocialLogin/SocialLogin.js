c6209ad5423eb9f21ea5be84cfcdc2882853a72e
Hamidul-1 <hamidul786cse@gmail.com>
2022-04-15 13:04:11 +0600

    SignInWithGoogle and error handle

--- src/Pages/Login/SocialLogin/SocialLogin.js
+++ src/Pages/Login/SocialLogin/SocialLogin.js
@@ -1,14 +1,46 @@
 import React from 'react';
+import google from '../../../images/social/googl-PNG.png'
+import facebook from '../../../images/social/facebook.png'
+import Github from '../../../images/social/GitHub.png'
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import auth from '../../../firebase.init';
+import { useNavigate } from 'react-router-dom';
 
 const SocialLogin = () => {
+    const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+    const navigate = useNavigate();
+    let errorElement;
+    if (error) {
+        errorElement = <div>
+            <p className='text-danger'>Error: {error.message}</p>
+        </div>
+    }
+
+    if (user) {
+        navigate('/home');
+    }
+
     return (
         <div>
             <div className='d-flex align-items-center'>
-                <div style={{height: '1px'}} className='bg-primary w-50'>
-
-                </div>
+                <div style={{ height: '1px' }} className='bg-primary w-50'></div>
                 <p className='mt-2 px-2'>or</p>
-                <div style={{height: '1px'}} className='bg-primary w-50'></div>
+                <div style={{ height: '1px' }} className='bg-primary w-50'></div>
+            </div>
+            {errorElement}
+            <div>
+                <button onClick={() => signInWithGoogle()} className='btn btn-info w-50 d-block mx-auto my-2'>
+                    <img style={{ width: '30px' }} src={google} alt="" />
+                    <span className='px-2'>Google Sign In</span>
+                </button>
+                <button className='btn btn-info w-50 d-block mx-auto my-2'>
+                    <img style={{ width: '30px' }} src={facebook} alt="" />
+                    <span className='px-2'>Facebook Sign In</span>
+                </button>
+                <button className='btn btn-info w-50 d-block mx-auto my-2'>
+                    <img style={{ width: '30px' }} src={Github} alt="" />
+                    <span className='px-2'>Github</span>
+                </button>
             </div>
         </div>
     );
