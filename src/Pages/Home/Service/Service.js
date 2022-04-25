27df2bd6fc3295354d13af9e38f6c1ecff0b133d
Hamidul-1 <hamidul786cse@gmail.com>
2022-04-25 11:34:55 +0600

    created requireAuth

--- src/Pages/Home/Service/Service.js
+++ src/Pages/Home/Service/Service.js
@@ -3,7 +3,7 @@ import { useNavigate } from 'react-router-dom';
 import './Service.css';
 
 const Service = ({service}) => {
-    const {id, name , price, description, img} = service;
+    const {_id, name , price, description, img} = service;
     const navigate = useNavigate();
 
     const navigateToServiceDetail = id =>{
@@ -15,7 +15,7 @@ const Service = ({service}) => {
             <h2>{name}</h2>
             <p>Price: {price}</p>
             <p><small>{description}</small></p>
-            <button onClick={()=> navigateToServiceDetail(id)} className='btn btn-primary'>Book: {name}</button>
+            <button onClick={()=> navigateToServiceDetail(_id)} className='btn btn-primary'>Book: {name}</button>
         </div>
     );
 };
--- src/Pages/Login/RequireAuth/RequireAuth.js
+++ src/Pages/Login/RequireAuth/RequireAuth.js
@@ -1,12 +1,14 @@
+import { sendEmailVerification } from 'firebase/auth';
 import React from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { Navigate, useLocation } from 'react-router-dom';
+import { toast, ToastContainer } from 'react-toastify';
 import auth from '../../../firebase.init';
 import Loading from '../../Shared/Loading/Loading';
 
 const RequireAuth = ({children}) => {
     const [user, loading] = useAuthState(auth);
-    console.log('inside require auth',user);
+    // console.log('inside require auth',user);
     const location = useLocation();
     if(loading){
         return <Loading></Loading>
@@ -14,6 +16,24 @@ const RequireAuth = ({children}) => {
 
     if(!user){
         return <Navigate to="/login" state={{from:location}} replace />;
+    }
+
+    // console.log(user);
+    if(user.providerId[0]?.providerId === 'password' && !user.emailVerified){
+        return <div className='text-center mt-5'>
+            <h3 className='text-danger'>Your Email is not verified!!</h3>
+            <h5 className='text-success'>Please Verify your email address </h5>
+            <button
+            className='btn btn-primary'
+            onClick={async () =>{
+                await sendEmailVerification();
+                toast('Send email');
+            }}
+            >
+                Send Verification Email Again
+            </button>
+            <ToastContainer></ToastContainer>
+        </div>
     }
     return children;
 };
